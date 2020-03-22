5f261b49917325b7567dabb44ed0939dc38c4440
Jeff King <jazfunk@gmail.com>
2020-03-22 16:41:18 -0400

    Some minor bug fixes.  Still can't read strikethrough property

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-
-   
-
 //  Button to add Task to list
-var addButton = document.getElementById('addBtnID').addEventListener('click', addTODO);
+var addButton = document
+  .getElementById("addBtnID")
+  .addEventListener("click", addTODO);
 
 // Get tasklist (ul called 'tasks'), and add event listener
-var taskList = document.getElementById('tasks');
-taskList.addEventListener('click', delTODO);
-
-
+var taskList = document.getElementById("tasks");
+taskList.addEventListener("click", delTODO);
 
 // Function to Add To-Do item to the list
 function addTODO(e) {
@@ -27,16 +24,10 @@ function addTODO(e) {
   delBtn.className = "btn-Delete";
   delBtn.appendChild(document.createTextNode("Delete"));
 
-  //   // Create Complete task button, ad class, append text node
-  //   var compBtn = document.createElement('button');
-  //   compBtn.className = 'btn-Complete';
-  //   compBtn.appendChild(document.createTextNode('Completed'));
-
-  // Appen buttons to li
+  // Appen button to li
   li.appendChild(delBtn);
-  //   li.appendChild(compBtn);
 
-  // Append i to list
+  // Append li to list
   taskList.appendChild(li);
 
   console.log("add to-do item to list");
@@ -47,7 +38,11 @@ function delTODO(e) {
     var li = e.target.parentElement;
     taskList.removeChild(li);
     console.log("deleted to-do item from list");
-    // Check if completed (strikethrough exists)
+
+    //  Can't seem to hook onto the textDecoration property
+    //  to determine whether task is already strikethrough and can be deleted.
+
+    //  Check if completed (strikethrough exists)
     // if (
     //   e.target.style.textDecoration == "line-through"
     // ) {
@@ -56,10 +51,10 @@ function delTODO(e) {
     //   console.log("deleted to-do item from list");
     // }
   } else {
-    // The text area was clicked, change text to strikthrough
+    // Not the delete button, strikethrough text, if not already done
     //console.log(e.target.innerText);
     var textToStrike = e.target.innerText;
-    if (e.target.style.textDecorationLine !== 'line-through') {
+    if (e.target.style.textDecoration !== "line-through") {
       var result = textToStrike.substring(0, textToStrike.length - 6);
       var buttonHTML =
         '<input type="submit" value="Delete" class="btn-Delete">';
@@ -67,9 +62,8 @@ function delTODO(e) {
       e.target.innerHTML = result.strike() + buttonHTML;
       console.log(e.target.innerHTML);
     } else {
-      console.log(e.targetstyle.textDecorationLine);
+      console.log(e.target.style.textDecoration);
       //alert('strikethrough encountered');
     }
   }
 }
-
