95f0acf94744b743d3722cf891ef4a37d23d76a1
Jeff King <jazfunk@gmail.com>
2020-03-22 20:28:51 -0400

    Cleaned up, bugs gone.  Still cannot delete only if complete.

--- script.js
+++ script.js
@@ -1,69 +1,59 @@
 //  Button to add Task to list
-var addButton = document
-  .getElementById("addBtnID")
-  .addEventListener("click", addTODO);
+var addButton = document.getElementById("addBtnID").addEventListener("click", addTODO);
 
 // Get tasklist (ul called 'tasks'), and add event listener
 var taskList = document.getElementById("tasks");
-taskList.addEventListener("click", delTODO);
+taskList.addEventListener("click", modTODO);
 
 // Function to Add To-Do item to the list
 function addTODO(e) {
   // Get Task from user input
   var newTask = document.getElementById("taskDescription").value;
 
-  // Create new li element, add className
-  var li = document.createElement("li");
-  li.className = "task-list-item";
+  // Check for empty strings, skip and do not notify user
+  if (newTask !== "") {
+    // Create new li element, add className
+    var li = document.createElement("li");
+    li.className = "task-list-item";
+    // Add Task text to the li element
+    li.appendChild(document.createTextNode(newTask));
 
-  // Add Task text to the li element
-  li.appendChild(document.createTextNode(newTask));
+    // Create delete button, ad class, append text node
+    var delBtn = document.createElement("button");
+    delBtn.className = "btn-Delete";
+    delBtn.appendChild(document.createTextNode("Delete"));
 
-  // Create delete button, ad class, append text node
-  var delBtn = document.createElement("button");
-  delBtn.className = "btn-Delete";
-  delBtn.appendChild(document.createTextNode("Delete"));
+    // Appen button to li
+    li.appendChild(delBtn);
 
-  // Appen button to li
-  li.appendChild(delBtn);
+    // Append li to list
+    taskList.appendChild(li);
 
-  // Append li to list
-  taskList.appendChild(li);
-
-  console.log("add to-do item to list");
+    console.log("add to-do item to list");
+  }
 }
 
-function delTODO(e) {
+function modTODO(e) {
   if (e.target.className === "btn-Delete") {
+    // Delete Button detected, delete accordingly
     var li = e.target.parentElement;
-    taskList.removeChild(li);
-    console.log("deleted to-do item from list");
-
-    //  Can't seem to hook onto the textDecoration property
-    //  to determine whether task is already strikethrough and can be deleted.
-
-    //  Check if completed (strikethrough exists)
-    // if (
-    //   e.target.style.textDecoration == "line-through"
-    // ) {
-    //   var li = e.target.parentElement;
+      taskList.removeChild(li);
+      console.log("deleted to-do item from list");
+    // Check first character, determine if line-through
+    //var liSubString = li.innerText.substring(0, 1);
+    // if (li.innerText.style.textDcoration !== "line-through") {
     //   taskList.removeChild(li);
     //   console.log("deleted to-do item from list");
     // }
   } else {
-    // Not the delete button, strikethrough text, if not already done
-    //console.log(e.target.innerText);
+    // Toggle Strike-through action detected
     var textToStrike = e.target.innerText;
     if (e.target.style.textDecoration !== "line-through") {
-      var result = textToStrike.substring(0, textToStrike.length - 6);
-      var buttonHTML =
-        '<input type="submit" value="Delete" class="btn-Delete">';
-
-      e.target.innerHTML = result.strike() + buttonHTML;
-      console.log(e.target.innerHTML);
+      e.target.style.textDecoration = "line-through";
     } else {
+      e.target.style.textDecoration = "";
       console.log(e.target.style.textDecoration);
-      //alert('strikethrough encountered');
     }
   }
 }
+
