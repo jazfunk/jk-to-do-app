2025b512e9478169b7b50837b5083f19879e7def
Jeff King <jazfunk@gmail.com>
2020-03-25 20:42:41 -0400

    Started working on localStorage. Not much success, at this point.

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 
+var savedTasks = new Object();
+
 document.getElementById("addBtnID").addEventListener("click", addTODO);
 var taskList = document.getElementById("tasks");
 taskList.addEventListener("click", modTODO);
 
-function addTODO(e) { 
+window.onload = function() {
+  this.browserSupport();
+  this.getLocalTaskList();
+};
+
+function addTODO(e) {
   var newTask = document.getElementById("taskDescription").value;
-  if (newTask !== "") {    
+  if (newTask !== "") {
     var li = document.createElement("li");
-    li.className = "task-list-item";    
-    li.appendChild(document.createTextNode(newTask));    
+    li.className = "task-list-item";
+    li.appendChild(document.createTextNode(newTask));
     var delBtn = document.createElement("button");
     delBtn.className = "btn-Delete";
-    delBtn.appendChild(document.createTextNode("Delete"));    
-    li.appendChild(delBtn);    
-    taskList.appendChild(li);        
-  } else {      
-      alert('You cannot add an empty task.')
+    delBtn.appendChild(document.createTextNode("Delete"));
+    li.appendChild(delBtn);
+    taskList.appendChild(li);
+    saveLocalTaskList(taskList);
+  } else {
+    alert("You cannot add an empty task.");
   }
 }
 
@@ -23,9 +31,9 @@ function modTODO(e) {
   if (e.target.className === "btn-Delete") {
     var li = e.target.parentElement;
     if (li.style.textDecoration === "line-through") {
-      taskList.removeChild(li);      
+      taskList.removeChild(li);
     } else {
-      alert('Cannot DELETE, task not marked COMPLETE.');
+      alert("Cannot DELETE, task is not marked COMPLETE.");
     }
   } else {
     var textToStrike = e.target.innerText;
@@ -35,4 +43,23 @@ function modTODO(e) {
       e.target.style.textDecoration = "";
     }
   }
+}
+
+function browserSupport() {
+  if (typeof Storage !== "undefined") {
+    // Code for localStorage
+  } else {
+    // No web storage Support.
+    alert(
+      "Your browser does not support local storage.  Cannot save your tasks."
+    );
+  }
+}
+
+function saveLocalTaskList(taskList) {
+  window.localStorage.setItem("task", JSON.stringify(taskList));  
+}
+
+function getLocalTaskList() {
+  savedTasks = JSON.parse(window.localStorage.getItem("task"));  
 }
