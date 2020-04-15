7729b75ea8fab7e923c3313cb2ac1381e7dcbce1
Jeff King <jazfunk@gmail.com>
2020-04-15 11:57:38 -0400

    Final commit, after implementing code review changes.

--- README.md
+++ README.md
@@ -5,21 +5,6 @@ Written entirely in pure JavaScript, HTML, and CSS, an app to create a list of t
 [View Application](https://jazfunk.github.io/jk-to-do-app/)  
 [View Repository](https://github.com/jazfunk/jk-to-do-app.git)
 
-## Instructions
-- Enter task description in text box.  
-- Click "Add Task", or press the ENTER key to add to list.  
-- Click anywhere in the description to mark complete.  
-- Completed items will change to text decoration to "line-through" (Strike Through).  
-"COMPLETED: " will be affixed to the beginning of the task description.  
-- Click anywhere in the description to toggle completion.
-- Click "Delete" button to delete a completed item.  
-- Items not marked complete cannot be deleted.  
-- All items will be saved locally.  
-- Subsequent visits show most recent activity at the bottom of the list. 
-- To clear all items listed and saved locally, click "Clear All Tasks".  
-- The last user action committed is displayed in the system message area above the to-do list.
-
-
 ## Application Images
 
 * **No List Items**<br>
@@ -30,10 +15,10 @@ Written entirely in pure JavaScript, HTML, and CSS, an app to create a list of t
 
 <!-- 
 * **No List Items**<br>
-![noListItems](https://raw.githubusercontent.com/jazfunk/jk-to-do-app/master/images/todoApp_SS_NoItems.PNG)<br>
+![noListItems](https://raw.githubusercontent.com/jazfunk/jk-to-do-app/master/images/todoApp_SS_NoItems.png)<br>
 
 * **List items added**<br>
-![listItems](https://raw.githubusercontent.com/jazfunk/jk-to-do-app/master/images/todoApp_SS_WithItems.PNG) -->
+![listItems](https://raw.githubusercontent.com/jazfunk/jk-to-do-app/master/images/todoApp_SS_WithItems.png) -->
 
 ## Summary
 Seemingly a simple application to develop, the "To-Do" app was a challenging one.  Utilizing local browser storage to persist data between user visits has always been something I wanted to become proficient in.  With the skills developed from building this app, my understanding of local storage has improved tremendously.
--- script.js
+++ script.js
@@ -1,127 +1,148 @@
+const DEFAULT_MESSAGE = "Welcome!";
+var taskList = document.getElementById("tasks");
 
-const defaultSystemMessage = "Welcome!"
-const id = "COMPLETED: "
-const itemBaseHTML = '<Button class="btn-delete">Delete</Button>';
-let isComplete = false;
-let savedTasks = [];
-let taskList = document.getElementById("tasks");
-taskList.addEventListener("click", modTODO);
+class Task {
+  constructor(text, isComplete) {
+    this.text = text;
+    this.isComplete = isComplete;
+  }
+}
 
-window.onload = function() {
-  document
-    .getElementById("add-btn-id")
-    .addEventListener("click", function(e) {      
-      addTODO(document.getElementById("task-description").value); 
-      e.preventDefault();               
-    });
+window.onload = function () {
+  document.getElementById("add-btn-id").addEventListener("click", function (e) {
+    const task = new Task(
+      document.getElementById("task-description").value,
+      false
+    );
+    renderToDo(task);
+    updateLocal(task);
+    e.preventDefault();
+  });
   document
     .getElementById("clear-local-btn")
-    .addEventListener("click", function(e) {
-      if (confirm("This will clear all listed and locally saved tasks, Are you sure?")) {
-        //document.getElementById("system-message").innerText = "";
+    .addEventListener("click", function (e) {
+      if (
+        confirm(
+          "This will clear all listed and locally saved tasks, Are you sure?"
+        )
+      ) {
         displaySystemMessage("All Items Cleared");
         document.getElementById("task-description").value = "";
         document.getElementById("task-description").focus();
         window.localStorage.clear();
-        savedTasks.length = 0;
         while (taskList.firstChild) {
           taskList.removeChild(taskList.firstChild);
-        }        
+        }
       }
     });
   browserSupport();
-  getLocalTaskList();  
-}
+  getLocalTaskList();
+};
 
-function updateLocal(task) {
-  if (task != "removed-item") {
-    savedTasks.push(task);
-  }
-  window.localStorage.setItem("tasks", JSON.stringify(savedTasks));
-  //document.getElementById("system-message").innerText = `"${task}" updated.`
-  isComplete = false;
-}
+function renderToDo(task) {
+  if (task.text !== "") {
+    document.getElementById(
+      "system-message"
+    ).innerText = `"${task.text}" added to list.`;
 
-function addTODO(task) {
-  if (task !== "") {
-    document.getElementById("system-message").innerText = `"${task}" added to list.`
     var li = document.createElement("li");
     li.className = "task-list-item";
+    li.style.textDecoration = task.isComplete ? "line-through" : "";
+    li.appendChild(document.createTextNode(task.text));
+    li.addEventListener("click", function (e) {
+      if (e.target.className === "task-list-item") {
+        if (e.target.style.textDecoration !== "line-through") {
+          e.target.style.textDecoration = "line-through";
+          task.isComplete = true;
+        } else {
+          e.target.style.textDecoration = "";
+          task.isComplete = false;
+        }
 
-    if (isComplete) {
-      li.style.textDecoration = "line-through";
-    } else {
-      li.style.textDecoration = "";
-    }
+        displaySystemMessage(
+          task.isComplete
+            ? `"${task.text}" Marked Complete`
+            : `"${task.text}" Marked  Not Complete`
+        );
+        updateLocalTask(task);
+      }
+    });
 
-    li.appendChild(document.createTextNode(task));
     var deletebutton = document.createElement("button");
     deletebutton.className = "btn-delete";
-    //deletebutton.disabled = true;
     deletebutton.appendChild(document.createTextNode("Delete"));
+    deletebutton.addEventListener("click", function (e) {
+      if (li.style.textDecoration === "line-through") {
+        removeLocalTask(task);
+        taskList.removeChild(li);
+        displaySystemMessage(`Removed Task: "${task.text}"`);
+        document.getElementById("task-description").focus();
+      } else {
+        displaySystemMessage("Cannot DELETE, task is not marked COMPLETE.");
+      }
+    });
+
     li.appendChild(deletebutton);
+
     taskList.appendChild(li);
-    updateLocal(task);
+
     document.getElementById("task-description").value = "";
   } else {
-    //document.getElementById("system-message").innerText = "You cannot add an empty task.";
-    displaySystemMessage('You cannot add an empty task.');
-    //alert("You cannot add an empty task.");
+    displaySystemMessage("You cannot add an empty task.");
   }
   document.getElementById("task-description").focus();
 }
 
-function modTODO(e) {
-  if (e.target.className === "btn-delete") {
-    var li = e.target.parentElement;
-    var task = li.innerText.substring(0, li.innerText.length - 7);
-    if (li.style.textDecoration === "line-through") {
-      removeLocalTask(task);
-        taskList.removeChild(li);
-        //document.getElementById("system-message").innerText = `Removed Task: "${task}"`
-        displaySystemMessage(`Removed Task: "${task}"`)
-        document.getElementById("task-description").focus();
-      // if (confirm("Are you sure? This cannot be undone.")) {
-      //   removeLocalTask(task);
-      //   taskList.removeChild(li);
-      //   //document.getElementById("system-message").innerText = `Removed Task: "${task}"`
-      //   displaySystemMessage(`Removed Task: "${task}"`)
-      //   document.getElementById("task-description").focus();
-      // }   
-    } else {
-      //alert("Cannot DELETE, task is not marked COMPLETE.");
-      displaySystemMessage('Cannot DELETE, task is not marked COMPLETE.')
-      //document.getElementById("system-message").innerText = "Cannot DELETE, task is not marked COMPLETE."
-    }
-  } else {
-    if (e.target.className === "task-list-item") {
-      if (e.target.style.textDecoration !== "line-through") {
-        e.target.style.textDecoration = "line-through";
-        isComplete = true;
-      } else {
-        e.target.style.textDecoration = "";
-        isComplete = false;
-      }
+function getLocalTaskList() {
+  var localTasks = JSON.parse(window.localStorage.getItem("tasks")) || [];
 
-      var targetTextLength = e.target.innerHTML.length;
-      targetTask = e.target.innerHTML.substring(
-        0,
-        targetTextLength - itemBaseHTML.length
-      );
-      // document.getElementById(
-      //   "system-message"
-      // ).innerText = `Task "${targetTask}" Complete = ${isComplete}`;
-      displaySystemMessage(
-        isComplete
-          ? `"${targetTask}" Marked Complete`
-          : `"${targetTask}" Marked  Not Complete`
-      );
-      task = addStrikeThroughID(targetTask);
-      e.target.innerHTML = task + itemBaseHTML;
-      removeLocalTask(targetTask);
-      updateLocal(task);
-    }
+  for (task in localTasks) {
+    renderToDo(localTasks[task]);
   }
+
+  displaySystemMessage(
+    localTasks.length > 0
+      ? `You have ${localTasks.length} item(s) in your To-Do list`
+      : DEFAULT_MESSAGE
+  );
+}
+
+function updateLocal(task) {
+  const savedTasks = JSON.parse(window.localStorage.getItem("tasks")) || [];
+  savedTasks.push(task);
+  window.localStorage.setItem("tasks", JSON.stringify(savedTasks));
+}
+
+// I've ended up with two functions doing basically the same thing.
+// This fx was reanamed from "updateLocal", before I realized there was
+// an "updateLocal" fx written above.
+// I'm leaving this and moving on and maybe come back to it another
+// time and see if I really need both of these... I'm certain I don't.
+// jk  2020-04-15
+function updateLocalTask(task) {
+  const savedTasks = JSON.parse(window.localStorage.getItem("tasks")) || [];
+  const matchingTask = savedTasks.find(function (savedTask) {
+    return task.text === savedTask.text;
+  });
+  const index = savedTasks.indexOf(matchingTask);
+  savedTasks[index] = task;
+  window.localStorage.setItem("tasks", JSON.stringify(savedTasks));
+}
+
+function removeLocalTask(task) {
+  const savedTasks = JSON.parse(window.localStorage.getItem("tasks")) || [];
+  const matchingTask = savedTasks.find(function (savedTask) {
+    return task.text === savedTask.text;
+  });
+  const index = savedTasks.indexOf(matchingTask);
+  if (index > -1) {
+    savedTasks.splice(index, 1);
+    window.localStorage.setItem("tasks", JSON.stringify(savedTasks));
+  }
+}
+
+function displaySystemMessage(systemMessage) {
+  document.getElementById("system-message").innerText = systemMessage;
 }
 
 function browserSupport() {
@@ -131,41 +152,4 @@ function browserSupport() {
     alert("Your browser does not support local storage.");
   }
   document.getElementById("task-description").focus();
-}
-
-function removeLocalTask(task) {
-  const index = savedTasks.indexOf(task);
-  if (index > -1) {
-    savedTasks.splice(index, 1);
-    updateLocal("removed-item");    
-  }
-}
-
-function getLocalTaskList() {
-  var localTasks = JSON.parse(window.localStorage.getItem("tasks"));
-  if (localTasks != null) {
-    for (task in localTasks) {      
-        isComplete = localTasks[task].substring(0, id.length) === id;  
-      addTODO(localTasks[task]);
-    }
-    displaySystemMessage(`You have ${localTasks.length} item(s) in your To-Do list`)
-  } else {
-    displaySystemMessage(defaultSystemMessage)
-    console.log("localStorage is empty");
-  }    
-}
-
-function addStrikeThroughID(task) {
-  const taskST_ID = task.substring(0, id.length);
-  if(taskST_ID === id) {
-    task = task.substring(id.length, task.length);
-  } else {
-    task = id + task
-    isComplete = true;
-  }
-  return task;
-}
-
-function displaySystemMessage(systemMessage) {
-  document.getElementById('system-message').innerText = systemMessage;
 }
